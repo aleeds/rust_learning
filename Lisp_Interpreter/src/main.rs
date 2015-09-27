32de1d5015453f4e25af570494474b3fdf4d4a33
Andy Leeds <aleeds@trinity.edu>
2015-09-27 16:24:19 -0700

    I added the parser. It turns valid LISP sentences into a SyntaxTree.

--- Lisp_Interpreter/src/main.rs
+++ Lisp_Interpreter/src/main.rs
@@ -1,15 +1,120 @@
 use std::io;
 
-enum SyntaxTree {
-    Expression(String),
-    Node(String,Vec<SyntaxTree>),
+struct Value {
+    iv : i32,
+    b_iv : bool,
+
 }
 
-// fn Parse(expr : &str) -> SyntaxTree {
-//
-// }
+impl Value {
+    fn new(v : &str) -> Value {
+        let v: i32 = v.trim().parse()
+         .ok()
+         .expect("Values must be a number");
 
-fn is_balanced(expr : String) -> bool {
+        Value {
+            iv : v,b_iv : true
+
+        }
+    }
+}
+
+enum SyntaxTree {
+    Err,
+    Val{v : Value},
+    Node{fnc : String, child : Vec<SyntaxTree>},
+}
+
+fn parse_to_vec(expr : &String) -> Vec<String> {
+  let mut v : Vec<String> = Vec::new();
+  let mut st = String::new();
+  for c in expr.chars() {
+    if c == '(' || c == ')' {
+      if !st.is_empty() {
+        v.push(st);
+      }
+      st = String::new();
+      st.push(c);
+      v.push(st);
+      st = String::new();
+    } else if c == ' ' {
+      if !st.is_empty() {
+        v.push(st);
+      }
+      st = String::new();
+    } else {
+      st.push(c);
+    }
+  }
+
+  v
+}
+
+fn several_vecs_to_trees(vs : Vec<Vec<String>>) -> Vec<SyntaxTree> {
+    let mut ret : Vec<SyntaxTree> = Vec::new();
+    for v in vs.iter() {
+        ret.push(vec_to_tree(v));
+    }
+    ret
+}
+
+fn vec_to_tree(tree : &Vec<String>) -> SyntaxTree {
+
+   for i in 0..(tree.len()) {
+       print!("{} ", tree[i]);
+   }
+   println!("");
+
+   let mut num_right = 0;
+   for c in tree.iter() {
+       if c.clone() == "(".to_string() {
+           num_right += 1;
+       }
+   }
+
+   if tree.len() == 1 {
+       return SyntaxTree::Val{v : Value::new(&tree[0])};
+   } else if num_right == 1 {
+      let mut vs : Vec<Vec<String>> = Vec::new();
+      for i in  2..(tree.len() - 1) {
+          vs.push(vec![tree[i].clone()]);
+      }
+      // need to find more elegant solution to this
+      let vs = several_vecs_to_trees(vs);
+      return SyntaxTree::Node{fnc : tree[1].clone(), child : vs};
+   } else {
+       let mut vecs : Vec<Vec<String>> = Vec::new();
+       let mut i = 2;
+       let mut right = 0;
+       let mut add_v : Vec<String> = Vec::new();
+       while i < tree.len() {
+           if tree[i] == "(".to_string() {
+               right += 1;
+           } else if tree[i] == ")".to_string() {
+               right += -1;
+           }
+           if right == 0 {
+               add_v.push(")".to_string());
+               vecs.push(add_v.clone());
+               add_v = Vec::new();
+           }
+           else {
+               add_v.push(tree[i].clone());
+           }
+           i += 1;
+       }
+       return SyntaxTree::Node{fnc : tree[1].clone(),child : several_vecs_to_trees(vecs)};
+
+   }
+   SyntaxTree::Err
+}
+
+fn parse_to_syntax_tree(expr : &String) -> SyntaxTree {
+    let v = parse_to_vec(&expr);
+    vec_to_tree(&v)
+}
+
+fn is_balanced(expr : &String) -> bool {
     let mut c = 0;
     for s in expr.chars() {
         if s == '(' {
@@ -19,9 +124,7 @@ fn is_balanced(expr : String) -> bool {
             c += -1;
         }
         if c < 0 {
-            // Why can't I just say false?
-            // That is what the book says, but it doesn't do that
-            return false;
+            break;
         }
     }
     c == 0
@@ -31,13 +134,17 @@ fn main() {
     let mut inp = String::new();
     // I need to change this so the loop will properly halt.
     // For now you exit program with ctrl c.
+
+
     while inp != "Exit" {
         io::stdin().read_line(&mut inp)
            .ok()
             .expect("Failed to read line");
         inp = String::from(inp.trim());
-        let is_b = is_balanced(inp.clone());
-        println!("  {}  {}",inp,is_b);
+        let is_b = is_balanced(&inp);
+        println!("{}",is_b);
+        let parsed = parse_to_vec(&inp);
+        vec_to_tree(&parsed);
         inp = String::from("");
     }
 }
